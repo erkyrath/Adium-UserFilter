3cb7b9d2a88a56e6a58c969f5fc72d57b2c55898
Andrew Plotkin <zarf@Zarfslab.local>
2016-08-22 22:03:09 -0400

    Replace copyright message (still GPL).

--- AISFPreferences.h
+++ AISFPreferences.h
@@ -1,7 +1,8 @@
 /* 
- * Adium SpamFilter Plugin - Blacklist annoying messages in Adium
- * Copyright (C) 2010 Thijs Alkemade
- * 
+ * Adium UserFilter Plugin - Blacklist annoying users in Adium
+ * Copyright 2016 by Andrew Plotkin
+ * Based on Adium SpamFilter Plugin - Copyright (C) 2010 Thijs Alkemade
+ *
  * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
