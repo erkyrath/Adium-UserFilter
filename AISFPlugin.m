996a6d0946ed0d653bf3709136d6356dce4356b8
Andrew Plotkin <zarf@Zarfslab.local>
2016-08-22 21:16:41 -0400

    Use relative path for objc/objc-class.h.

--- AISFPlugin.m
+++ AISFPlugin.m
@@ -19,7 +19,8 @@
 #import <Adium/AIContentMessage.h>
 
 #import <Adium/AIPreferenceControllerProtocol.h>
-#import </usr/include/objc/objc-class.h>
+//#import </usr/include/objc/objc-class.h>
+#import <objc/objc-class.h>
 
 @class AIMedia;
 
